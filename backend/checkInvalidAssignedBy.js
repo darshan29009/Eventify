97ab34c75b7c584ac070d9b0ac62a0e87cf4b432
darshan patel <155099122+darshan29009@users.noreply.github.com>
2026-05-01 13:37:03 +0530

    Update checkInvalidAssignedBy.js

--- backend/checkInvalidAssignedBy.js
+++ backend/checkInvalidAssignedBy.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const runCheck = async () => {
   try {
-    const mongoURI = 'mongodb+srv://dmpatel299:darshan123@eventcraft.gwdcyr1.mongodb.net/eventify?retryWrites=true&w=majority';
+    const mongoURI = 'your url';
     await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
